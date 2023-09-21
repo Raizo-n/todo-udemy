0149a50d314ac30f19beaa7970b34279b0606054
“Raizo-n” <sanpincha0826@gmail.com>
2023-09-20 22:29:35 -0700

    add: css and outline of overall

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,35 @@
+// import React from "react";
 import "./App.css";
 
 export const App = () => {
-  return null;
+  return (
+    <>
+      <div className="input-area">
+        <input type="text" placeholder="Type a task..." />
+        <button>Add</button>
+      </div>
+
+      <div className="incomplete-area">
+        <p className="title">Undone TODO</p>
+        <ul className="flex">
+          <li>Yooo</li>
+          <button>Done</button>
+          <button>Delete</button>
+        </ul>
+        <ul className="flex">
+          <li>Sup</li>
+          <button>Done</button>
+          <button>Delete</button>
+        </ul>
+      </div>
+
+      <div className="complete-area">
+        <p className="title">Undone TODO</p>
+        <ul className="flex">
+          <li>Epic</li>
+          <button>Cancel</button>
+        </ul>
+      </div>
+    </>
+  );
 };
